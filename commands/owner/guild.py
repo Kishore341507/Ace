3e690dce1f18a5f36fdeab8362f07b52be939301
Kishore341507 <choudhary341507@gmail.com>
2024-01-11 10:18:56 +0530

    guild auto load

--- commands/owner/guild.py
+++ commands/owner/guild.py
@@ -1,10 +1,28 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 from database import *
 
 class Guild(commands.Cog):
 
     def __init__(self , client):
         self.client = client
+        self.load_guilds.start()
+
+    @tasks.loop( seconds=10 , count=1)
+    async def load_guilds(self):
+        for guild in self.client.guilds :
+            print(guild.id)
+            if guild.id not in self.client.data :
+                try :
+                    await self.client.db.execute('INSERT INTO guilds(id) VALUES ($1)' , guild.id)
+                except Exception as e :
+                    pass
+                finally :
+                    guild_data = await self.client.db.fetchrow('SELECT * FROM guilds WHERE id = $1' , guild.id)
+                    client.data[guild.id] = dict(guild_data)
+    
+    @load_guilds.before_loop
+    async def before_load_guilds(self):
+        await self.client.wait_until_ready()
 
     @commands.Cog.listener()
     async def on_guild_join(self ,  guild):  
@@ -17,22 +35,7 @@ class Guild(commands.Cog):
             pass
         finally :
             guild_data = await self.client.db.fetchrow('SELECT * FROM guilds WHERE id = $1' , guild.id)
-            client.data[guild.id] = dict(guild_data) 
-        
-    @commands.command()
-    @commands.is_owner()
-    async def loadguilds(self , ctx):
-        for guild in client.guilds :
-            if guild.id not in client.data :
-                try :
-                    await client.db.execute('INSERT INTO guilds(id) VALUES ($1)' , guild.id)
-                except Exception as e :
-                    pass
-                finally :
-                    guild_data = await self.client.db.fetchrow('SELECT * FROM guilds WHERE id = $1' , guild.id)
-                    client.data[guild.id] = dict(guild_data) 
-        await ctx.send("Done")
-        
+            client.data[guild.id] = dict(guild_data)         
 
 async def setup(client):
    await client.add_cog(Guild(client))         
