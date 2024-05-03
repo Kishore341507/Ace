5aabdcc39c9350d7017b7e8a843b0f1f053f8556
Ashborn <s.kharoud6757@outlook.com>
2024-05-02 20:30:39 -0400

    Added 0 in the game

--- commands/games/roulette.py
+++ commands/games/roulette.py
@@ -13,7 +13,7 @@ class roulette_space(commands.Converter):
             argument = int(argument)
         except :
             pass    
-        if argument not in ["odd" , "even","red" , "black","1-18" , "19-36","1st", "2nd", "3rd","1-12" , "13-24" , "25-36",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]:
+        if argument not in ["odd" , "even","red" , "black","1-18" , "19-36","1st", "2nd", "3rd","1-12" , "13-24" , "25-36",0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]:
             raise commands.BadArgument("Invalid space to bet upon.") 
         else :
             return argument         
@@ -23,7 +23,7 @@ class Roulette(commands.Cog):
 
     def __init__(self , client ):
         self.client = client
-        self.game = [{'number': 1, 'colour': 'red'}, {'number': 2, 'colour': 'black'}, {'number': 3, 'colour': 'red'}, {'number': 4, 'colour': 'black'}, {'number': 5, 'colour': 'red'}, {'number': 6, 'colour': 'black'}, {'number': 7, 'colour': 'red'}, {'number': 8, 'colour': 'black'}, {'number': 9, 'colour': 'red'}, {'number': 10, 'colour': 'black'}, {'number': 11, 'colour': 'black'}, {'number': 12, 'colour': 'red'}, {'number': 13, 'colour': 'black'}, {'number': 14, 'colour': 'red'}, {'number': 15, 'colour': 'black'}, {'number': 16, 'colour': 'red'}, {'number': 17, 'colour': 'black'}, {'number': 18, 'colour': 'red'}, {'number': 19, 'colour': 'red'}, {'number': 20, 'colour': 'black'}, {'number': 21, 'colour': 'red'}, {'number': 22, 'colour': 'black'}, {'number': 23, 'colour': 'red'}, {'number': 24, 'colour': 'black'}, {'number': 25, 'colour':'red'}, {'number': 26, 'colour': 'black'}, {'number': 27, 'colour': 'red'}, {'number': 28, 'colour': 'black'}, {'number': 29, 'colour': 'black'}, {'number': 30, 'colour': 'red'}, {'number': 31, 'colour': 'black'}, {'number': 32, 'colour': 'red'}, {'number': 33, 'colour': 'black'}, {'number': 34, 'colour': 'red'}, {'number': 35, 'colour': 'black'}, {'number': 36, 'colour': 'red'}]
+        self.game = [{'number' : 0, 'colour' : ''}, {'number': 1, 'colour': 'red'}, {'number': 2, 'colour': 'black'}, {'number': 3, 'colour': 'red'}, {'number': 4, 'colour': 'black'}, {'number': 5, 'colour': 'red'}, {'number': 6, 'colour': 'black'}, {'number': 7, 'colour': 'red'}, {'number': 8, 'colour': 'black'}, {'number': 9, 'colour': 'red'}, {'number': 10, 'colour': 'black'}, {'number': 11, 'colour': 'black'}, {'number': 12, 'colour': 'red'}, {'number': 13, 'colour': 'black'}, {'number': 14, 'colour': 'red'}, {'number': 15, 'colour': 'black'}, {'number': 16, 'colour': 'red'}, {'number': 17, 'colour': 'black'}, {'number': 18, 'colour': 'red'}, {'number': 19, 'colour': 'red'}, {'number': 20, 'colour': 'black'}, {'number': 21, 'colour': 'red'}, {'number': 22, 'colour': 'black'}, {'number': 23, 'colour': 'red'}, {'number': 24, 'colour': 'black'}, {'number': 25, 'colour':'red'}, {'number': 26, 'colour': 'black'}, {'number': 27, 'colour': 'red'}, {'number': 28, 'colour': 'black'}, {'number': 29, 'colour': 'black'}, {'number': 30, 'colour': 'red'}, {'number': 31, 'colour': 'black'}, {'number': 32, 'colour': 'red'}, {'number': 33, 'colour': 'black'}, {'number': 34, 'colour': 'red'}, {'number': 35, 'colour': 'black'}, {'number': 36, 'colour': 'red'}]
         self.players = []
 
     async def check_channel(ctx) -> bool:
